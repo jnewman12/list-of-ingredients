dfab2e3e3cffbdfb0fc2a10a420799a77367d479
James Newman <jamesmnewman1@gmail.com>
2018-08-26 18:48:31 -0700

    adding models, relationships, one more line in config.rb

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,10 @@ module ListOfIngredients
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+
     config.api_only = true
+
+    config.app_generators.scaffold_controller = :scaffold_controller
 
     # Middleware for ActiveAdmin
     config.middleware.use Rack::MethodOverride
